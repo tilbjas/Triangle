92e80e214e45ab74ac037973f54dd05bf24012d2
tilbjas <tilbjas@gmail.com>
2016-06-01 10:54:43 +0000

    Service, Windows Service and Test Project
    Couldnt get the Windows service app to work on my setup due to .NET adpater issues. So ran Service as one VS instance, and used another instance to run the test project.
    
    The test project is very simple at the moment. I want to add a data source to run through multiple test data sets

---
+++ TriangleTest/TriangleTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriangleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ServiceReference1.Service1Client sc = new ServiceReference1.Service1Client();
+
+           ServiceReference1.RightAngletriangle RHT = sc.getRHT(3, 2);
+
+           double area = sc.getArea(RHT);
+
+           Assert.AreEqual(area, (3 * 2 / 2));
+        }
+    }
+}
